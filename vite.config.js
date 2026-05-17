2b9fc4e2290dc1d975335bafaf3cef5acdc317b0
Dmytro Serhiienko <conneciones@gmail.com>
2026-05-17 18:17:30 +0300

    url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/gsap-mojito-practice/", // Додаємо цей рядок
+  base: "/mojito-bar/", // Додаємо цей рядок
   plugins: [react(), tailwindcss()],
 });
